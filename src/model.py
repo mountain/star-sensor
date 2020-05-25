428af37fc856dad10a28ec381a2e206a65938365
Mingli Yuan <mingli.yuan@gmail.com>
2020-05-25 16:27:27 +0800

    fix kernel size

--- src/model.py
+++ src/model.py
@@ -28,7 +28,7 @@ class Estimator(nn.Module):
         norm_layer = nn.InstanceNorm2d
         self._norm_layer = norm_layer
         layers = [2, 2, 2, 2]
-        num_classes = 12
+        num_classes = 36
         inchannel = 13
 
         self.inplanes = 64
@@ -42,7 +42,7 @@ class Estimator(nn.Module):
                              "or a 3-element tuple, got {}".format(replace_stride_with_dilation))
         self.groups = groups
         self.base_width = width_per_group
-        self.conv1 = nn.Conv2d(inchannel, self.inplanes, kernel_size=61, stride=2, padding=30,
+        self.conv1 = nn.Conv2d(inchannel, self.inplanes, kernel_size=7, stride=2, padding=3,
                                bias=False)
         self.bn1 = norm_layer(self.inplanes)
         self.relu = nn.ReLU(inplace=True)
@@ -116,9 +116,14 @@ class Estimator(nn.Module):
         y = self.avgpool(y)
         y = th.flatten(y, 1)
         y = self.fc(y)
-        y = self.softmax(y).view(-1, 12, 1, 1)
 
-        return self.ones * y
+        p = y[:, 0:12]
+        p = self.softmax(p).view(-1, 12, 1, 1)
+        a = y[:, 12:24]
+        b = y[:, 24:36]
+        alpha = th.arctan2(a, b)
+        
+        return th.cat((self.ones * p, self.ones * th.cos(alpha), self.ones * th.sin(alpha)), dim=1)
 
 
 class Locator(nn.Module):
@@ -129,10 +134,10 @@ class Locator(nn.Module):
         norm_layer = nn.InstanceNorm2d
         self._norm_layer = norm_layer
         layers = [2, 2, 2, 2]
-        num_classes = 12
-        inchannel = 15
+        num_classes = 4
+        inchannel = 39
 
-        self.inplanes = 64
+        self.inplanes = 96
         self.dilation = 1
         if replace_stride_with_dilation is None:
             # each element in the tuple indicates if we should replace
@@ -143,21 +148,20 @@ class Locator(nn.Module):
                              "or a 3-element tuple, got {}".format(replace_stride_with_dilation))
         self.groups = groups
         self.base_width = width_per_group
-        self.conv1 = nn.Conv2d(inchannel, self.inplanes, kernel_size=61, stride=2, padding=30,
+        self.conv1 = nn.Conv2d(inchannel, self.inplanes, kernel_size=7, stride=2, padding=3,
                                bias=False)
         self.bn1 = norm_layer(self.inplanes)
         self.relu = nn.ReLU(inplace=True)
         self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
-        self.layer1 = self._make_layer(block, 64, layers[0])
-        self.layer2 = self._make_layer(block, 128, layers[1], stride=2,
+        self.layer1 = self._make_layer(block, 96, layers[0])
+        self.layer2 = self._make_layer(block, 192, layers[1], stride=2,
                                        dilate=replace_stride_with_dilation[0])
-        self.layer3 = self._make_layer(block, 256, layers[2], stride=2,
+        self.layer3 = self._make_layer(block, 384, layers[2], stride=2,
                                        dilate=replace_stride_with_dilation[1])
-        self.layer4 = self._make_layer(block, 512, layers[3], stride=2,
+        self.layer4 = self._make_layer(block, 768, layers[3], stride=2,
                                        dilate=replace_stride_with_dilation[2])
         self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
-        self.fc = nn.Linear(512 * block.expansion, num_classes)
-        self.softmax = nn.Softmax()
+        self.fc = nn.Linear(768 * block.expansion, num_classes)
 
         for m in self.modules():
             if isinstance(m, nn.Conv2d):
@@ -216,8 +220,7 @@ class Locator(nn.Module):
         y = th.flatten(y, 1)
         y = self.fc(y)
 
-
-        return y.view(1, 12, 1)
+        return y.view(1, 4)
 
 
 class Model(nn.Module):
@@ -259,30 +262,25 @@ class Model(nn.Module):
         v11 = self.skyview(q11).view(1, 1, 512, 512)
         v12 = self.skyview(q12).view(1, 1, 512, 512)
 
-        self.icosahedron = th.cat((q01, q02, q03, q04,
-                                   q05, q06, q07, q08,
-                                   q09, q10, q11, q12), dim=0).view(1, 12, 4)
-        self.icosahedron.requires_grad=False
-
-        self.icosahedron_view = th.cat((v01, v02, v03, v04,
+        self.icosahedron = th.cat((v01, v02, v03, v04,
                                    v05, v06, v07, v08,
                                    v09, v10, v11, v12), dim=1)
-        self.icosahedron_view.requires_grad = False
+        self.icosahedron.requires_grad = False
 
     def forward(self, x):
         batch = x.size()[0]
 
-        estimate = self.estimator(th.cat((x, self.icosahedron_view), dim=1))
+        estimate = self.estimator(th.cat((x, self.icosahedron), dim=1))
 
-        q1 = normalize(th.sum(self.icosahedron * self.locator(th.cat((x, self.init, self.init, estimate), dim=1)), dim=1).view(batch, 4))
+        q1 = normalize(self.locator(th.cat((x, self.init, self.init, estimate), dim=1)).view(batch, 4))
         qa = q1
         s1 = self.skyview(qa).view(batch, 1, 512, 512)
 
-        q2 = normalize(th.sum(self.icosahedron * self.locator(th.cat((x, s1, self.init, estimate), dim=1)), dim=1).view(batch, 4))
+        q2 = normalize(th.sum(self.locator(th.cat((x, s1, self.init, estimate), dim=1)).view(batch, 4))
         qa = hamilton_product(q2, qa)
         s2 = self.skyview(qa).view(batch, 1, 512, 512)
 
-        q3 = normalize(th.sum(self.icosahedron * self.locator(th.cat((x, s2, s1, estimate), dim=1)), dim=1).view(batch, 4))
+        q3 = normalize(self.locator(th.cat((x, s2, s1, estimate), dim=1)).view(batch, 4))
         qa = hamilton_product(q3, qa)
         s3 = self.skyview(qa).view(batch, 1, 512, 512)
 
