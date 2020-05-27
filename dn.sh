bd7bad179307cd1167b4efa4a4553c8d272278c6
Mingli Yuan <mingli.yuan@gmail.com>
2020-05-27 18:44:51 +0800

    rename model

--- dn.sh
+++ dn.sh
@@ -2,9 +2,3 @@ scp elderberry.in.caiyunapp.com:~/star-sensor/0.png .
 scp elderberry.in.caiyunapp.com:~/star-sensor/1.png .
 scp elderberry.in.caiyunapp.com:~/star-sensor/2.png .
 scp elderberry.in.caiyunapp.com:~/star-sensor/3.png .
-scp elderberry.in.caiyunapp.com:~/star-sensor/4.png .
-scp elderberry.in.caiyunapp.com:~/star-sensor/5.png .
-scp elderberry.in.caiyunapp.com:~/star-sensor/6.png .
-scp elderberry.in.caiyunapp.com:~/star-sensor/7.png .
-scp elderberry.in.caiyunapp.com:~/star-sensor/8.png .
-scp elderberry.in.caiyunapp.com:~/star-sensor/9.png .
