81e2c899774e7574a8735d075f2b9b731071f0f5
Mingli Yuan <mingli.yuan@gmail.com>
2020-06-01 20:29:35 +0800

    by dopri5

--- src/util/multiscale.py
+++ src/util/multiscale.py
@@ -7,12 +7,12 @@ from torch.nn import functional as F
 class MultiscaleMSELoss(Module):
     def __init__(self):
         super(MultiscaleMSELoss, self).__init__()
-        self.maxpool = MaxPool2d(3, stride=2)
+        self.maxpool = MaxPool2d(3, stride=2, padding=1)
 
     def forward(self, input, target):
         i, t, l = input, target, 0
-        while i.size()[-1] > 1 and i.size()[-2] > 1:
+        while i.size()[-1] >= 1 and i.size()[-2] >= 1:
             print(i.size())
-            l += F.mse_loss(input, target, reduction='mean')
+            l += F.mse_loss(i, t, reduction='mean')
             i, t = self.maxpool(i), self.maxpool(t)
         return l
