4a623f78e7d6a55daa448bdc2238cc1d6e59787d
mingli <mingli.yuan@gmail.com>
2020-06-01 15:45:50 +0800

    try a exp seeking strategy

--- src/model.py
+++ src/model.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 
+import numpy as np
 import torch as th
 import torch.nn as nn
 import logging
@@ -17,103 +18,6 @@ logger = logging.getLogger()
 def normalize(q):
     p = q_normalize(q)
     return p
-
-
-class Estimator(nn.Module):
-
-    def __init__(self, zero_init_residual=False, groups=1, width_per_group=64, replace_stride_with_dilation=None):
-        super(Estimator, self).__init__()
-        block = BasicBlock
-        norm_layer = nn.InstanceNorm2d
-        self._norm_layer = norm_layer
-        layers = [2, 2, 2, 2]
-        num_classes = 73
-        inchannel = num_classes + 1
-
-        self.inplanes = 144
-        self.dilation = 1
-        if replace_stride_with_dilation is None:
-            # each element in the tuple indicates if we should replace
-            # the 2x2 stride with a dilated convolution instead
-            replace_stride_with_dilation = [False, False, False]
-        if len(replace_stride_with_dilation) != 3:
-            raise ValueError("replace_stride_with_dilation should be None "
-                             "or a 3-element tuple, got {}".format(replace_stride_with_dilation))
-        self.groups = groups
-        self.base_width = width_per_group
-        self.conv1 = nn.Conv2d(inchannel, self.inplanes, kernel_size=7, stride=2, padding=3,
-                               bias=False)
-        self.bn1 = norm_layer(self.inplanes)
-        self.relu = nn.ReLU(inplace=True)
-        self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
-        self.layer1 = self._make_layer(block, 144, layers[0])
-        self.layer2 = self._make_layer(block, 288, layers[1], stride=2,
-                                       dilate=replace_stride_with_dilation[0])
-        self.layer3 = self._make_layer(block, 576, layers[2], stride=2,
-                                       dilate=replace_stride_with_dilation[1])
-        self.layer4 = self._make_layer(block, 1152, layers[3], stride=2,
-                                       dilate=replace_stride_with_dilation[2])
-        self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
-        self.fc = nn.Linear(1152 * block.expansion, num_classes)
-
-        for m in self.modules():
-            if isinstance(m, nn.Conv2d):
-                nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='relu')
-            elif isinstance(m, (nn.BatchNorm2d, nn.GroupNorm)):
-                nn.init.constant_(m.weight, 1)
-                nn.init.constant_(m.bias, 0)
-
-        # Zero-initialize the last BN in each residual branch,
-        # so that the residual branch starts with zeros, and each residual block behaves like an identity.
-        # This improves the model by 0.2~0.3% according to https://arxiv.org/abs/1706.02677
-        if zero_init_residual:
-            for m in self.modules():
-                if isinstance(m, Bottleneck):
-                    nn.init.constant_(m.bn3.weight, 0)
-                elif isinstance(m, BasicBlock):
-                    nn.init.constant_(m.bn2.weight, 0)
-
-    def _make_layer(self, block, planes, blocks, stride=1, dilate=False):
-        norm_layer = self._norm_layer
-        downsample = None
-        previous_dilation = self.dilation
-        if dilate:
-            self.dilation *= stride
-            stride = 1
-        if stride != 1 or self.inplanes != planes * block.expansion:
-            downsample = nn.Sequential(
-                conv1x1(self.inplanes, planes * block.expansion, stride),
-                norm_layer(planes * block.expansion),
-            )
-
-        layers = []
-        layers.append(block(self.inplanes, planes, stride, downsample, self.groups,
-                            self.base_width, previous_dilation, norm_layer))
-        self.inplanes = planes * block.expansion
-        for _ in range(1, blocks):
-            layers.append(block(self.inplanes, planes, groups=self.groups,
-                                base_width=self.base_width, dilation=self.dilation,
-                                norm_layer=norm_layer))
-
-        return nn.Sequential(*layers)
-
-    def forward(self, x):
-        # See note [TorchScript super()]
-        y = self.conv1(x)
-        y = self.bn1(y)
-        y = self.relu(y)
-        y = self.maxpool(y)
-
-        y = self.layer1(y)
-        y = self.layer2(y)
-        y = self.layer3(y)
-        y = self.layer4(y)
-
-        y = self.avgpool(y)
-        y = th.flatten(y, 1)
-        y = self.fc(y)
-
-        return y
 
 
 class Locator(nn.Module):
@@ -210,7 +114,7 @@ class Locator(nn.Module):
         y = th.flatten(y, 1)
         y = self.fc(y)
 
-        return y.view(-1, 4)
+        return normalize(y.view(-1, 4))
 
 
 class Flow(nn.Module):
@@ -218,34 +122,33 @@ class Flow(nn.Module):
     def __init__(self, skyview):
         super().__init__()
         self.skyview = skyview
-        self.estimator = Estimator()
         self.locator = Locator()
-        self.icosahedron = Icosahedron(skyview)
 
     def qtarget(self, y):
         self.target = y
 
     def qinit(self, y):
         batch = y.size()[0]
-        quaternions, views = self.icosahedron()
-        quaternions = quaternions.expand(batch, -1, -1)
-        views = views.expand(batch, -1, -1, -1)
-        estimate = self.estimator(th.cat((y, views), dim=1)).view(-1, 73, 1)
-        return normalize(th.sum(quaternions * estimate, dim=1))
+        theta = th.random(batch, 1) * 2 * np.pi
+        phi = (th.random(batch, 1) - 0.5) * np.pi
+        alpha = th.random(batch, 1) * 2 * np.pi
+        a = th.cos(alpha / 2)
+        b = th.sin(alpha / 2) * th.sin(phi) * th.cos(theta)
+        c = th.sin(alpha / 2) * th.sin(phi) * th.sin(theta)
+        d = th.sin(alpha / 2) * th.cos(phi)
 
-    def qvelocity(self, curr, trgt):
-        q = self.locator(th.cat((self.qview(curr), trgt), dim=1)).view(-1, 4)
-        q = hamilton_product(q, curr)
-        q = q - th.sum(curr * q) / get_modulus(q) * curr
-        return q
+        return th.cat((a, b, c, d), dim=1)
 
     def qview(self, q):
         view = self.skyview(q).view(-1, 1, 512, 512)
         #logger.info(f'view: {view.max().item():0.6f} {view.min().item():0.6f} {view.mean().item():0.6f}')
         return view
 
+    def qvelocity(self, qcurr, vtrgt):
+        qdelt = self.locator(th.cat(self.qview(qcurr), vtrgt)).view(-1, 4)
+        return normalize(qdelt) * th.exp(get_modulus(qdelt))
+
     def forward(self, t, q):
-        #logger.info(f't: {t.item():0.4f}')
         return self.qvelocity(q, self.target)
 
 
@@ -259,7 +162,7 @@ class Model(nn.Module):
     def forward(self, x):
         self.flow.qtarget(x)
         q0 = self.flow.qinit(x)
-        qs = odeint(self.flow, q0, th.arange(0.0, 1.01, 0.25), method='dopri5', rtol=1e-2, atol=1e-2)
+        qs = odeint(self.flow, q0, th.arange(0.0, 6.01, 0.25), method='dopri5', rtol=1e-2, atol=1e-2)
 
         vn3 = self.flow.qview(qs[-3])
         vn2 = self.flow.qview(qs[-2])
