139dc23ae219db1fb4664f07647019ab18cd5abe
Mingli Yuan <mingli.yuan@gmail.com>
2020-06-01 21:14:45 +0800

    by dopri5

--- dn.sh
+++ dn.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
-scp elderberry.in.caiyunapp.com:~/star-sensor/0.png .
-scp elderberry.in.caiyunapp.com:~/star-sensor/1.png .
-scp elderberry.in.caiyunapp.com:~/star-sensor/2.png .
-scp elderberry.in.caiyunapp.com:~/star-sensor/3.png .
+scp elderberry.in.caiyunapp.com:~/star-sensor/o.png .
+scp elderberry.in.caiyunapp.com:~/star-sensor/t.png .
+scp elderberry.in.caiyunapp.com:~/star-sensor/r.png .
