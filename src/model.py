3990f855ef1e14d43cb7ed27991a275340b959c1
mingli <mingli.yuan@gmail.com>
2020-06-03 01:18:13 +0800

    normalization

--- src/model.py
+++ src/model.py
@@ -5,27 +5,57 @@ import torch as th
 import torch.nn as nn
 import logging
 
-from qnn.quaternion_ops import hamilton_product
 from unet.base import Swish
-from util.sky import Skyview
+from util.sky import Skyview, cast
 from torchdiffeq import odeint_adjoint as odeint
 
 
 logger = logging.getLogger()
 
+epsilon = 1e-7
 
-def length(q):
+I = cast(np.array([[0, 1, 0, 0]], dtype=np.float32))
+J = cast(np.array([[0, 0, 1, 0]], dtype=np.float32))
+K = cast(np.array([[0, 0, 0, 1]], dtype=np.float32))
+
+
+def norm(q):
     return th.sqrt(th.sum(q * q, dim=1, keepdim=True))
 
 
 def normalize(q):
-    return q / length(q)
+    return q / norm(q)
+
+
+def hamilton_product(x, y):
+    a1, b1, c1, d1 = x[:, 0], x[:, 1], x[:, 2], x[:, 3]
+    a2, b2, c2, d2 = y[:, 0], y[:, 1], y[:, 2], y[:, 3]
+
+    a3 = a1 * a2 - b1 * b2 - c1 * c2 - d1 * d2
+    b3 = a1 * b2 + b1 * c2 + c1 * d2 - d1 * c2
+    c3 = a1 * c2 - b1 * d2 + c1 * a2 + d1 * b2
+    d3 = a1 * d2 + b1 * c2 - c1 * b2 - d1 * a2
+
+    return th.cat((a3, b3, c3, d3), dim=1)
+
+
+def conjugate(q):
+    return - (
+        q + hamilton_product(I, hamilton_product(q, I)) + \
+        hamilton_product(J, hamilton_product(q, J)) + \
+        hamilton_product(K, hamilton_product(q, K))
+    ) / 2.0
+
+
+def reciprocal(q):
+    return conjugate(q) / (th.sum(q * q, dim=1, keepdim=True) + epsilon)
 
 
 class Net(nn.Module):
 
-    def __init__(self, inchannel, num_classes):
+    def __init__(self, inchannel):
         super(Net, self).__init__()
+
         self.relu = Swish()
         self.conv1 = nn.Conv2d(inchannel, 16, kernel_size=3, stride=2, padding=1, bias=False)
         self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
@@ -36,7 +66,7 @@ class Net(nn.Module):
         self.conv6 = nn.Conv2d(256, 512, kernel_size=3, stride=2, padding=1)
         self.conv7 = nn.Conv2d(512, 1024, kernel_size=3, stride=2, padding=1)
         self.conv8 = nn.Conv2d(1024, 2048, kernel_size=3, stride=2, padding=1)
-        self.fc = nn.Linear(2048, num_classes)
+        self.fc = nn.Linear(2048, 4)
 
         for m in self.modules():
             if isinstance(m, nn.Conv2d):
@@ -76,16 +106,16 @@ class Flow(nn.Module):
         self.vtarget = v
 
     def qview(self, q):
-        view = self.skyview(q.view(-1, 4)).view(-1, 1, 512, 512)
+        view = self.skyview(q).view(-1, 1, 512, 512)
         return view
 
     def tangent(self, qcurr, qtrgt):
-        return qtrgt - th.sum(qtrgt * qcurr, dim=1, keepdim=True) / length(qcurr) / length(qtrgt) * qcurr
+        return qtrgt - th.sum(qtrgt * qcurr, dim=1, keepdim=True) / norm(qcurr) / norm(qtrgt) * qcurr
 
     def forward(self, t, q):
-        estim = self.estimator(th.cat((self.qview(q), self.vtarget), dim=1)).view(-1, 2, 4)
-        delta = hamilton_product(hamilton_product(normalize(estim[:, 0]), estim[:, 1]), q)
-        return self.tangent(q, normalize(q + delta))
+        p = normalize(self.estimator(th.cat((self.qview(q), self.vtarget), dim=1)))
+        g = normalize(hamilton_product(hamilton_product(p, q), reciprocal(p)))
+        return self.tangent(q, g)
 
 
 class Model(nn.Module):
@@ -93,7 +123,7 @@ class Model(nn.Module):
     def __init__(self):
         super().__init__()
         self.skyview = Skyview()
-        self.estimator = Net(2, 8)
+        self.estimator = Net(2, 4)
         self.flow = Flow(self.skyview, self.estimator)
 
     def qinit(self, y):
