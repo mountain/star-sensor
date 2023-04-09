e328f642afe6d9df9a9525d30ea26a49790ec637
mingli <mingli.yuan@gmail.com>
2023-04-09 12:48:07 +0800

    add tests

--- test/test_sky.py
+++ test/test_sky.py
@@ -1,3 +1,4 @@
+import numpy as np
 import util.sky as sky
 import util.plotter as plt
 
@@ -6,45 +7,49 @@ plt.plot(
     open('polar.png', 'wb'),
     view.reshape(1, 512, 512)
 )
-for angle in range(0, 390, 30):
-    view = sky.skyview(0.0, 90.0, angle)
-    plt.plot(
-        open('polar-%03d.png' % angle, 'wb'),
-        view.reshape(1, 512, 512)
-    )
 
 view = sky.skyview(82.0, 0.0, 0.0)
 plt.plot(
     open('orion.png', 'wb'),
     view.reshape(1, 512, 512)
 )
-for angle in range(0, 390, 30):
-    view = sky.skyview(82.0, 0.0, angle)
-    plt.plot(
-        open('orion-%03d.png' % angle, 'wb'),
-        view.reshape(1, 512, 512)
-    )
 
 view = sky.skyview(248.0, -26.0, 0.0)
 plt.plot(
     open('scorpius.png', 'wb'),
     view.reshape(1, 512, 512)
 )
-for angle in range(0, 390, 30):
-    view = sky.skyview(248.0, -26.0, angle)
-    plt.plot(
-        open('scorpius-%03d.png' % angle, 'wb'),
-        view.reshape(1, 512, 512)
-    )
 
 view = sky.skyview(182.0, 58.0, 0.0)
 plt.plot(
     open('ursamajor.png', 'wb'),
     view.reshape(1, 512, 512)
 )
+
+
+trace = []
 for angle in range(0, 390, 30):
-    view = sky.skyview(182.0, 58.0, angle)
-    plt.plot(
-        open('ursamajor-%03d.png' % angle, 'wb'),
-        view.reshape(1, 512, 512)
-    )
+    trace.append(sky.skyview(16.49 * 15, -26.4319, angle))
+trace = np.sum(trace, axis=0)
+plt.plot(
+    open('antares.png', 'wb'),
+    trace.reshape(1, 512, 512)
+)
+
+trace = []
+for angle in range(0, 390, 30):
+    trace.append(sky.skyview(5.1995 * 15, 7.4070, angle))
+trace = np.sum(trace, axis=0)
+plt.plot(
+    open('betelgeuse.png', 'wb'),
+    trace.reshape(1, 512, 512)
+)
+
+trace = []
+for angle in range(0, 390, 30):
+    trace.append(sky.skyview(2.5302 * 15, 89.2641, angle))
+trace = np.sum(trace, axis=0)
+plt.plot(
+    open('polaris.png', 'wb'),
+    trace.reshape(1, 512, 512)
+)
