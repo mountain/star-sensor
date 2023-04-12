ae6c37cfbfa6f6ad100d065735fb546f1d1a11ae
Mingli Yuan <mingli.yuan@gmail.com>
2023-04-13 07:13:14 +0800

    revert baseline

--- src/models/v1.py
+++ src/models/v1.py
@@ -1,55 +1,32 @@
 import torch as th
 import pytorch_lightning as pl
 
-from torch import nn
 from torch.nn import functional as F
 
+from torchvision.models.resnet import ResNet, Bottleneck
 from util.config import hnum, vnum, device
 
 
-class Baseline(pl.LightningModule):
+class RegressionResNet(ResNet):
+    def __init__(self) -> None:
+        super().__init__(Bottleneck, [3, 4, 6, 3], 3, False, 1)
+
+
+class Model(pl.LightningModule):
     def __init__(self):
         super().__init__()
-        self.relu = nn.ReLU()
-        self.dnsample = nn.UpsamplingBilinear2d(scale_factor=0.5)
-        self.predicter = nn.Sequential(
-            nn.Conv2d(4, 8, kernel_size=3, padding=2, dtype=th.float32),
-            self.relu,
-            self.dnsample,
-            nn.Conv2d(8, 16, kernel_size=3, padding=2, dtype=th.float32),
-            self.relu,
-            self.dnsample,
-            nn.Conv2d(16, 32, kernel_size=3, padding=2, dtype=th.float32),
-            self.relu,
-            self.dnsample,
-            nn.Conv2d(32, 64, kernel_size=3, padding=2, dtype=th.float32),
-            self.relu,
-            self.dnsample,
-            nn.Conv2d(64, 128, kernel_size=3, padding=2, dtype=th.float32),
-            self.relu,
-            self.dnsample,
-            nn.Conv2d(128, 256, kernel_size=3, padding=2, dtype=th.float32),
-            self.relu,
-            self.dnsample,
-            nn.Conv2d(256, 512, kernel_size=3, padding=2, dtype=th.float32),
-            self.relu,
-            self.dnsample,
-            nn.Flatten(),
-            nn.Linear(61952, 3, dtype=th.float32),
-            nn.Tanh()
-        )
 
         g0 = th.linspace(-1, 1, hnum, requires_grad=False, dtype=th.float32)
         g1 = th.linspace(-1, 1, vnum, requires_grad=False, dtype=th.float32)
         grid = th.cat(th.meshgrid([g0, g1]), dim=1).reshape(1, 2, hnum, vnum)
         r = th.sqrt(grid[:, 0:1] * grid[:, 0:1] + grid[:, 1:2] * grid[:, 1:2])
-        a = th.atan2(grid[:, 0:1], grid[:, 1:2])
-        c = th.cos(a)
-        s = th.sin(a)
-        self.constants = th.cat([r, s, c], dim=1).reshape(1, 3, hnum, vnum).to(device)
+        a = th.atan2(grid[:, 0:1], grid[:, 1:2]) / th.pi
+        self.constants = th.cat([r, a], dim=1).reshape(1, 2, hnum, vnum).to(device)
 
-    def forward(self, sky):
-        result = self.predicter(sky) * 180
+        self.resnet = RegressionResNet()
+
+    def forward(self, input):
+        result = 180 * th.tanh(self.resnet(input))
         theta, phi, alpha = result[:, 0:1] + 180, result[:, 1:2] / 2, result[:, 2:3]
         return theta, phi, alpha
 
@@ -59,7 +36,7 @@ class Baseline(pl.LightningModule):
 
     def training_step(self, train_batch, batch_idx):
         theta, phi, alpha, sky = train_batch
-        constants = self.constants.view(-1, 4, hnum, vnum)
+        constants = self.constants.view(-1, 2, hnum, vnum)
         sky = sky.view(-1, 1, hnum, vnum)
         data = th.cat([sky, constants * th.ones_like(sky[:, 0:1])], dim=1)
         theta_hat, phi_hat, alpha_hat = self(data)
@@ -74,7 +51,7 @@ class Baseline(pl.LightningModule):
         theta, phi, alpha, sky = val_batch
         sky = sky.view(-1, 1, hnum, vnum)
         theta, phi, alpha, sky = theta, phi, alpha, sky
-        constants = self.constants.view(-1, 4, hnum, vnum)
+        constants = self.constants.view(-1, 2, hnum, vnum)
         data = th.cat([sky, constants * th.ones_like(sky[:, 0:1])], dim=1)
         theta_hat, phi_hat, alpha_hat = self(data)
         loss_theta = F.mse_loss(theta_hat.view(-1, 1), theta.view(-1, 1))
@@ -86,5 +63,6 @@ class Baseline(pl.LightningModule):
 
 
 def _model_():
-    return Baseline()
+    return Model()
+
 
