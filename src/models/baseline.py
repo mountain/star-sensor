2e2519256c99ec37979f779e4c7f84a703e42f1d
Mingli Yuan <mingli.yuan@gmail.com>
2023-04-13 13:02:01 +0800

    fix kernel

--- src/models/baseline.py
+++ src/models/baseline.py
@@ -13,13 +13,13 @@ class Baseline(pl.LightningModule):
         self.relu = nn.ReLU()
         self.dnsample = nn.UpsamplingBilinear2d(scale_factor=0.5)
         self.sensor = nn.Sequential(
-            nn.Conv2d(4, 8, kernel_size=48, padding=25, dtype=th.float32),
+            nn.Conv2d(4, 8, kernel_size=24, padding=13, dtype=th.float32),
             self.relu,
             self.dnsample,
-            nn.Conv2d(8, 16, kernel_size=48, padding=25, dtype=th.float32),
+            nn.Conv2d(8, 16, kernel_size=24, padding=13, dtype=th.float32),
             self.relu,
             self.dnsample,
-            nn.Conv2d(16, 32, kernel_size=48, padding=25, dtype=th.float32),
+            nn.Conv2d(16, 32, kernel_size=24, padding=13, dtype=th.float32),
             self.relu,
             self.dnsample,
             nn.Conv2d(32, 64, kernel_size=24, padding=13, dtype=th.float32),
